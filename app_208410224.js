7fdafbd1c97d54a0296ca192aee4ceb26c67666d
changiojen1 <208410224@gms.tku.edu.tw>
2023-03-09 19:15:21 +0800

    W04-P1-1 add 3 images of my meals to menu arra

---
+++ app_208410224.js
@@ -0,0 +1,65 @@
+const menu = [
+    {
+        id: 1,
+        title:'toasted bread',
+        category: 'breakfast',
+        price: 12,
+        img: './images/my-1.jpg',
+        remote_img: '',
+        desc: "I love toast! I love how easy it is to prepare in the morning and it’s so yummy. I like it with grilled tomatoes also and with coffee!",
+    },
+    {
+        id: 2,
+        title:'salad',
+        category: 'lunch',
+        price: 15,
+        img: './images/my-2.jpg',
+        remote_img: '',
+        desc: "I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed",
+
+    },
+    {
+        id: 3,
+        title:'fried rice',
+        category: 'dinner',
+        price: 18,
+        img: './images/my-3.jpg',
+        remote_img: '',
+        desc: "I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed",
+
+    },
+    {
+        id: 4,
+        title:'buttermilk pancakes',
+        category: 'breakfast',
+        price: 15.99,
+        img: './images/item-1.jpeg',
+        remote_img: '',
+        desc: "I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed",
+
+    },
+    {
+        id: 5,
+        title:'dinner double',
+        category: 'dinner',
+        price: 13.99,
+        img: './images/item-2.jpeg',
+        remote_img: '',
+        desc: "I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed",
+
+    },
+    {
+        id: 6,
+        title:'godzilla milkshake',
+        category: 'shakes',
+        price: 13.99,
+        img: './images/item-3.jpeg',
+        remote_img: '',
+        desc: "I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed",
+
+    },
+]
+
+const btnContainer = document.querySelector('.btn-container')
+const sectionCenter = document.querySelector('.section-container')
+
