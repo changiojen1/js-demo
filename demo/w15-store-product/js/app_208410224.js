b619c18a76bc1b43f094a4e29d1818ed217fdcec
changiojen1 <208410224@gms.tku.edu.tw>
2023-05-25 21:21:15 +0800

    W15-P2: get menu items from local json (products.json)

---
+++ demo/w15-store-product/js/app_208410224.js
@@ -0,0 +1,55 @@
+const url = `https://course-api.com/javascript-store-products`;
+
+const container = document.querySelector(".products-container");
+const btns = document.querySelectorAll(".company-btn");
+let data;
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const response = await fetch(url);
+  data = await response.json();
+  console.log(data);
+  displayProducts(data);
+});
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const company = e.currentTarget.innerHTML;
+    const filterData = productsFilter(company);
+    displayProducts(filterData);
+  });
+});
+
+const displayProducts = (products) => {
+  const displayData = products
+    .map((product) => {
+      const { name, price, image } = product.fields;
+      // console.log("image url", image[0].url);
+      return `
+        <div class="single-product">
+              <img
+                src="${image[0].url}"
+                class="single-product-img img"
+                alt="${name}"
+              />
+              <footer>
+                <h5 class="name">${name}</h5>
+                <span class="price">$${price}</span>
+              </footer>
+            </div>
+    `;
+    })
+    .join("");
+  container.innerHTML = displayData;
+};
+
+const productsFilter = (company) => {
+  console.log("company", company);
+  const filterData = data.filter((product) => {
+    if (product.fields.company === company) {
+      return product;
+    } else if (company === "all") {
+      return product;
+    }
+  });
+  return filterData;
+};
---
+++ demo/w15-store-product/js/app_local_208410224.js
@@ -0,0 +1,55 @@
+const url = `./api/products.json`;
+
+const container = document.querySelector(".products-container");
+const btns = document.querySelectorAll(".company-btn");
+let data;
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const response = await fetch(url);
+  data = await response.json();
+  console.log(data);
+  displayProducts(data);
+});
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const company = e.currentTarget.innerHTML;
+    const filterData = productsFilter(company);
+    displayProducts(filterData);
+  });
+});
+
+const displayProducts = (products) => {
+  const displayData = products
+    .map((product, index) => {
+      const { name, price } = product.fields;
+      let image = `./images/product-${index + 1}.jpg`;
+      return `
+        <div class="single-product">
+              <img
+                src="${image}"
+                class="single-product-img img"
+                alt="${name}"
+              />
+              <footer>
+                <h5 class="name">${name}</h5>
+                <span class="price">$${price}</span>
+              </footer>
+            </div>
+    `;
+    })
+    .join("");
+  container.innerHTML = displayData;
+};
+
+const productsFilter = (company) => {
+  console.log("company", company);
+  const filterData = data.filter((product) => {
+    if (product.fields.company === company) {
+      return product;
+    } else if (company === "all") {
+      return product;
+    }
+  });
+  return filterData;
+};
