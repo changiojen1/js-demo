1d8147dd91ce3d433439049134917bbc39ce27eb
changiojen1 <208410224@gms.tku.edu.tw>
2023-06-01 21:39:40 +0800

    W16-P2: Show single meal by its ID

--- demo/w16-meal-funder/script_208410224.js
+++ demo/w16-meal-funder/script_208410224.js
@@ -26,7 +26,7 @@ const searchMeal = (e) => {
                     <h3>${meal.strMeal}</h3>
                     </div>
                 </div>
-                `
+              `
             )
             .join("");
         }
@@ -36,4 +36,76 @@ const searchMeal = (e) => {
   }
 };
 
+const getMealById = (mealID) => {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+    .then((response) => response.json())
+    .then((data) => {
+      // console.log("data", data);
+      const meal = data.meals[0];
+      addMealToDOM(meal);
+    });
+};
+
+const addMealToDOM = (meal) => {
+  const ingredients = [];
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+      );
+    } else {
+      break;
+    }
+    // console.log("ingredients", ingredients);
+  }
+  console.log("ingredients", ingredients);
+  single_mealEl.innerHTML = `
+    <div class="single-meal">
+        <h1>${meal.strMeal}</h1>
+        <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+        <div class="single-meal-info">
+            ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ""}
+            ${meal.strArea ? `<p>${meal.strArea}</p>` : ""}
+        </div>
+        <div class="main">
+            <p>${meal.strInstructions}</p>
+            <h2>Ingredients</h2>
+            <ul>
+                ${ingredients
+                  .map((ingredient) => `<li>${ingredient}</li>`)
+                  .join("")}
+            </ul>
+        </div>
+    </div>
+  `;
+};
+
+const randomMeal = () => {
+  fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+    .then((response) => response.json())
+    .then((data) => {
+      // console.log("data", data);
+      const meal = data.meals[0];
+      addMealToDOM(meal);
+    });
+};
+
 submit.addEventListener("submit", searchMeal);
+random.addEventListener("click", randomMeal);
+
+mealsEl.addEventListener("click", (e) => {
+  // console.log("e.path", e.composedPath());
+  const composedPath = e.composedPath();
+  const mealInfo = composedPath.find((item) => {
+    if (item.classList) {
+      return item.classList.contains("meal-info");
+    } else {
+      return false;
+    }
+  });
+  if (mealInfo) {
+    // console.log("mealInfo", mealInfo);
+    const mealID = mealInfo.getAttribute("data-mealid");
+    getMealById(mealID);
+  }
+});
